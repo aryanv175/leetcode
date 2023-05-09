dd1405166d2e5d6328837df6a82794e8bc8141be
aryanv175 <91381804+aryanv175@users.noreply.github.com>
2023-05-09 16:20:08 +0200

    Update solution.py

--- 1480-Running-Sum-of-1d-Array-main/solution.py
+++ 1480-Running-Sum-of-1d-Array-main/solution.py
@@ -1,3 +1,7 @@
+# 1480-Running-Sum-of-1d-Array-main 
+""" Logic: One loop that goes through the array
+           adds the previous element to the 
+           current one. """
 
 def runningSum(nums: List[int]) -> List[int]:
     for i in range(1, len(nums)):
