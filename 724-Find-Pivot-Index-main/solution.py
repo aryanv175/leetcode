a7a8f47de391eafed990fdae57d67b439068661a
aryanv175 <91381804+aryanv175@users.noreply.github.com>
2023-05-09 16:16:51 +0200

    Update solution.py

--- 724-Find-Pivot-Index-main/solution.py
+++ 724-Find-Pivot-Index-main/solution.py
@@ -1,12 +1,20 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        total = 0
-        for i in range(len(nums)):
-            total += nums[i]
-        left, right = 0, 0
-        for j in range(len(nums)):
-            right =  total - nums[j] - left
-            if left == right:
-                return j
-            left += nums[j]
-        return - 1
+# 724-Find-Pivot-Index-main 
+""" Logic: Have two sum values, left sum and 
+           right sum, iterate throught the  
+           entire array assuming that the current
+           element is the pivot index until one
+           value actually is found out to be the 
+           pivot index. Once it is found, return
+           the index value. """
+
+def pivotIndex(self, nums):
+    total = 0
+    for i in range(len(nums)):
+        total += nums[i]
+    left, right = 0, 0
+    for j in range(len(nums)):
+        right =  total - nums[j] - left
+        if left == right:
+            return j
+        left += nums[j]
+    return - 1
