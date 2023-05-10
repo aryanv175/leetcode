81ba0cd6c3f02adb7eb6b4c46e2d97e428893ef3
aryanv175 <91381804+aryanv175@users.noreply.github.com>
2023-05-10 15:21:47 +0200

    Update solution.py

--- 206-Reverse-Linked-List/solution.py
+++ 206-Reverse-Linked-List/solution.py
@@ -3,7 +3,17 @@ class ListNode:
    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
+# 206-Reverse-Linked-List
+""" Logic: Use a loop to go through the linked
+           lists, for every iteration we point
+           the current element to the previous 
+           one and the next element to the
+           current one, that way, in the end the
+           linkedlist is reversed.
+           First: 1 -> 2 -> 3
+           Second: None <- 1 -> 2 -> 3
+           Third: None <- 1 <- 2 -> 3
+           and so on."""
 def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
    prev = None
