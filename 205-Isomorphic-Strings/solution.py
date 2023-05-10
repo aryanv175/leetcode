041bfd598853b30d8789528fd8c161932ad1b8eb
aryanv175 <91381804+aryanv175@users.noreply.github.com>
2023-05-10 15:00:31 +0200

    Update solution.py

--- 205-Isomorphic-Strings/solution.py
+++ 205-Isomorphic-Strings/solution.py
@@ -1,3 +1,14 @@
+# 205-Isomorphic-Strings
+""" Logic: Used dictionaries in this problem. 
+           go through every letter in any of the
+           two strings, for every unique letter in s
+           assign it the first letter in that is at
+           the same index and store in the dictionariy. 
+           Once the letter in s is repeated check if the 
+           letter in t at that index is the same one,
+           if so, keep going. Otherwise, return False.
+           """
+
 def isIsomorphic(self, s: str, t: str) -> bool:
       taken = {}
       taken2 = {}
