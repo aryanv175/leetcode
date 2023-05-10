0398c6adf496d63993c700fd9600c3a683722d22
aryanv175 <91381804+aryanv175@users.noreply.github.com>
2023-05-10 15:13:59 +0200

    Update solution.py

--- 206-Reverse-Linked-List/solution.py
+++ 206-Reverse-Linked-List/solution.py
@@ -4,19 +4,19 @@ class ListNode:
         self.val = val
         self.next = next
 
-  def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-      prev = None
-      curr = head
+   prev = None
+   curr = head
 
-      if  curr == None:
-          return None
+   if  curr == None:
+       return None
 
-      while curr:
-          nex = curr.next
-          curr.next = prev
-          prev = curr
-          curr = nex
+   while curr:
+       nex = curr.next
+       curr.next = prev
+       prev = curr
+       curr = nex
 
 
-      return prev
+   return prev
