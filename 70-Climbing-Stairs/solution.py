0c8d61f94e74bd283029e56dd07278e57a7ad5e6
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-18 23:54:32 +0200

    Update solution.py

--- 70-Climbing-Stairs/solution.py
+++ 70-Climbing-Stairs/solution.py
@@ -1,3 +1,9 @@
+# 70. Climbing Stairs
+""" Logic: In this problem you are basically 
+           meant to figure out, in how many 
+           ways can you add up one or two to 
+           get to the required number."""
+           
 def climbStairs(n: int) -> int:
     if n == 1:
         return 1
