efe4569311d0668a4bd74cdcde88a8b344cb4dc8
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-18 23:43:09 +0200

    Update solution.py

--- 70-Climbing-Stairs/solution.py
+++ 70-Climbing-Stairs/solution.py
@@ -1,11 +1,11 @@
 def climbStairs(n: int) -> int:
-if n == 1:
-    return 1
-a1 = 1
-a2 = 1
-num = 0
-for i in range(n-1):
-    num = a1 + a2
-    a1 = a2 
-    a2 = num
-return num    
+    if n == 1:
+        return 1
+    a1 = 1
+    a2 = 1
+    num = 0
+    for i in range(n-1):
+        num = a1 + a2
+        a1 = a2 
+        a2 = num
+    return num    
