65fbbbd60fd26aae49c7c936f80ff12bc5038d7b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-18 23:43:01 +0200

    Update solution.py

--- 70-Climbing-Stairs/solution.py
+++ 70-Climbing-Stairs/solution.py
@@ -1,8 +1,11 @@
-def climbStairs(self, n: int) -> int:
-        if n<=2: return n
-        dp = [0]*(n+1)
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3,n+1):
-            dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
+def climbStairs(n: int) -> int:
+if n == 1:
+    return 1
+a1 = 1
+a2 = 1
+num = 0
+for i in range(n-1):
+    num = a1 + a2
+    a1 = a2 
+    a2 = num
+return num    
