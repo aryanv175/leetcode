cbd3d9b931f800d0d86dc6c71d94479025aa9954
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-20 23:51:20 +0200

    Create solution.py

---
+++ 424-Longest-Repeating-Character-Replacement/solution.py
@@ -0,0 +1,17 @@
+
+def characterReplacement( s, k):
+    count = {}
+    res = 0
+
+    left = 0
+
+    for right  in range(len(s)):
+        count[s[right]] = 1 + count.get(s[right], 0)
+
+        while right-left + 1 - max(count.values()) > k:
+            count[s[left]] -= 1
+            left += 1
+
+        res = max(res, right-left + 1)
+
+    return res
