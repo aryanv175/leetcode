c825c0dd665fbf930d0a01b3f38a5db6a63596b7
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-05-21 00:01:27 +0200

    Update solution.py

--- 424-Longest-Repeating-Character-Replacement/solution.py
+++ 424-Longest-Repeating-Character-Replacement/solution.py
@@ -1,10 +1,14 @@
-
-def characterReplacement( s, k):
+#424. Longest Repeating Character Replacement
+"""Logic: Here we will use a hashmap count which will store the 
+        occurrences of the characters in a substring. We can then substract 
+        the max count with the length of the substring at that will give us 
+        the number of characters we have to replace. Then we can confirm that 
+        that value is less than k. Then we will use two pointers, left and right 
+        to determine the length of the max substring we can take."""
+def characterReplacement(s, k) -> int:
     count = {}
     res = 0
-
     left = 0
-
     for right  in range(len(s)):
         count[s[right]] = 1 + count.get(s[right], 0)
 
@@ -13,5 +17,4 @@ def characterReplacement( s, k):
             left += 1
 
         res = max(res, right-left + 1)
-
     return res
