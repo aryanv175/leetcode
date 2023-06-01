440b9a93039099edc7b9510fbc4931bf6c6b2c5e
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-06-01 03:04:35 +0200

    Create solution.py

---
+++ Level 2/226-Invert-Binary-Tree/solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:  # base case, if it is empty return None
+            return None
+        
+        # swap the two elemts. left and right using a temporaty variable temp
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        # use recursion to invert the entire tree
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root # return root!
