4189f13b6cbe1dd4a3769e40704b4072da0fd29b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2023-06-02 01:03:17 +0200

    Create solution.py

---
+++ Level 2/437-Path-Sum-III/solution.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], target: int) -> int:
+        self.total = 0
+
+        def helper(node, cur):
+            if not node: 
+                return 
+            
+            helper(node.left, cur + node.val)
+            helper(node.right, cur + node.val)
+
+            if cur + node.val == target:
+                self.total += 1
+            
+        def dfs( node):
+            if not node:
+                return 
+
+            helper(node, 0)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return self.total
